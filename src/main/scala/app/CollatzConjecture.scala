f383143af414f46dcf1dbd5af7863944d5445b4f
Brian Wakenshaw <brian.wakenshaw@digital.hmrc.gov.uk>
2018-02-27 08:02:26 +0000

    add more tests and correct fuction name

--- src/main/scala/app/CollatzConjecture.scala
+++ src/main/scala/app/CollatzConjecture.scala
@@ -2,14 +2,13 @@ package app
 
 object CollatzConjecture extends App {
 
-  def apply(number: Int): Int = Collatz(number).length
+  def apply(number: Int): Int = collatz(number).length
 
-  def Collatz(number: Int): List[Int] = {
+  def collatz(number: Int): List[Int] = {
     number match {
       case n if n == 1 => List(1)
-      case n if n % 2 == 0 => number :: Collatz(number / 2)
-      case _ => number :: Collatz(number * 3 + 1)
+      case n if n % 2 == 0 => number :: collatz(number / 2)
+      case _ => number :: collatz(number * 3 + 1)
     }
-
   }
 }
--- src/test/scala/CollatzConjectureSpec.scala
+++ src/test/scala/CollatzConjectureSpec.scala
@@ -5,6 +5,17 @@ class CollatzConjectureSpec extends WordSpec with MustMatchers {
 
   "CollatzConjecture" must {
 
+    "return 1 when given 1" in {
+      CollatzConjecture(1) mustEqual 1
+    }
+
+    "return 2 when given 2" in {
+      CollatzConjecture(2) mustEqual 2
+    }
+
+    "return 8 when given 3" in {
+      CollatzConjecture(3) mustEqual 8
+    }
     "return 8 when given 20" in {
       CollatzConjecture(20) mustEqual 8
     }
